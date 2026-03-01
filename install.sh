29217edb889b7a161e55fa7043c3cbcf35859191
Brenton Horne <brentonhorne77@gmail.com>
2026-03-01 21:48:41 +1000

    +plotbtso

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.local/share/applications
-ln -sf $PWD/{julia,plotbts}.desktop ~/.local/share/applications
+ln -sf $PWD/{julia,plotbts,plotbtso}.desktop ~/.local/share/applications
 mkdir -p ~/.local/bin/
 ln -sf $PWD/plotbts ~/.local/bin/
+ln -sf $PWD/plotbtso ~/.local/bin
