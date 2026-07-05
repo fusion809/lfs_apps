1b4614295df02a3459cdfbdc1b6bf0241263284a
Brenton Horne <brentonhorne77@gmail.com>
2026-07-05 14:44:51 +1000

    Generalizing wallpaper changing scripts for KDE

--- cycle-wallpaper-previous.sh
+++ cycle-wallpaper-previous.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 WALLPAPER_DIR="$HOME/wallpapers"
 STATE_FILE="$HOME/.local/state/wallpaper_index"
-
+ERROR_LOG="$HOME/.local/state/wallpaper_previous.log"
+export PATH=$PATH:/opt/qt6/bin
 mkdir -p "$(dirname "$STATE_FILE")"
 
 mapfile -t WALLPAPERS < <(find "$WALLPAPER_DIR" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) | sort)
@@ -9,6 +10,7 @@ mapfile -t WALLPAPERS < <(find "$WALLPAPER_DIR" -maxdepth 1 -type f \( -iname "*
 NUM_WALLPAPERS=${#WALLPAPERS[@]}
 
 if [ "$NUM_WALLPAPERS" -eq 0 ]; then
+    echo "NUM_WALLPAPERS=0" > $ERROR_LOG
     exit 1
 fi
 
@@ -23,9 +25,20 @@ fi
 
 WALLPAPER_PATH="${WALLPAPERS[$INDEX]}"
 URI="file://$WALLPAPER_PATH"
-
-gsettings set org.gnome.desktop.background picture-uri "$URI"
-gsettings set org.gnome.desktop.background picture-uri-dark "$URI"
-
-PREVIOUS_INDEX=$(( (INDEX - 1) % NUM_WALLPAPERS ))
-echo "$PREVIOUS_INDEX" > "$STATE_FILE"
+echo "WALLPAPER_PATH=$WALLPAPER_PATH" >> $ERROR_LOG
+if [[ $XDG_SESSION_DESKTOP == "GNOME" ]]; then
+	gsettings set org.gnome.desktop.background picture-uri "$URI"
+	gsettings set org.gnome.desktop.background picture-uri-dark "$URI"
+elif [[ $XDG_SESSION_DESKTOP == "KDE" ]] ; then
+qdbus org.kde.plasmashell /PlasmaShell \
+            org.kde.PlasmaShell.evaluateScript "
+            var ds = desktops();
+            for (var i = 0; i < ds.length; i++) {
+                ds[i].wallpaperPlugin = 'org.kde.image';
+                ds[i].currentConfigGroup =
+                    ['Wallpaper', 'org.kde.image', 'General'];
+                ds[i].writeConfig('Image', '$URI');
+            }"
+fi
+NEXT_INDEX=$(( (INDEX - 1) % NUM_WALLPAPERS ))
+echo "$NEXT_INDEX" > "$STATE_FILE"
--- cycle-wallpaper-shuffle.sh
+++ cycle-wallpaper-shuffle.sh
@@ -2,7 +2,7 @@
 WALLPAPER_DIR="$HOME/wallpapers"
 STATE_FILE="$HOME/.local/state/wallpaper_index"
 ERROR_LOG="$HOME/.local/state/wallpaper_shuffle.log"
-
+export PATH=$PATH:/opt/qt6/bin
 mkdir -p "$(dirname "$STATE_FILE")"
 
 mapfile -t WALLPAPERS < <(find "$WALLPAPER_DIR" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) | sort)
@@ -25,10 +25,20 @@ fi
 
 WALLPAPER_PATH="${WALLPAPERS[$INDEX]}"
 URI="file://$WALLPAPER_PATH"
-echo "WALLPAPER_PATH=$WALLPAPER_PATH" > $ERROR_LOG
-
-gsettings set org.gnome.desktop.background picture-uri "$URI"
-gsettings set org.gnome.desktop.background picture-uri-dark "$URI"
-
+echo "WALLPAPER_PATH=$WALLPAPER_PATH" >> $ERROR_LOG
+if [[ $XDG_SESSION_DESKTOP == "GNOME" ]]; then
+	gsettings set org.gnome.desktop.background picture-uri "$URI"
+	gsettings set org.gnome.desktop.background picture-uri-dark "$URI"
+elif [[ $XDG_SESSION_DESKTOP == "KDE" ]] ; then
+qdbus org.kde.plasmashell /PlasmaShell \
+            org.kde.PlasmaShell.evaluateScript "
+            var ds = desktops();
+            for (var i = 0; i < ds.length; i++) {
+                ds[i].wallpaperPlugin = 'org.kde.image';
+                ds[i].currentConfigGroup =
+                    ['Wallpaper', 'org.kde.image', 'General'];
+                ds[i].writeConfig('Image', '$URI');
+            }"
+fi
 NEXT_INDEX=$(R -q -e "sample.int($NUM_WALLPAPERS+1, 1)-1" | grep "^\[1\]" | cut -d ' ' -f 2)
 echo "$NEXT_INDEX" > "$STATE_FILE"
